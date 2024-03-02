31419b3f64f327df1c62b955c2eb5b5d39ee6a8b
kylat15 <112619520+kylat15@users.noreply.github.com>
2024-03-02 17:34:43 +0800

    first commit

---
+++ App.tsx
@@ -0,0 +1,38 @@
+import * as React from "react";
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Onboarding0 from './screens/Onboarding0';
+import Onboarding1 from './screens/Onboarding1';
+import Onboarding2 from './screens/Onboarding2';
+import { useFonts } from "expo-font";
+
+const Stack = createStackNavigator();
+
+export default function App() {
+  const [fontsLoaded, error] = useFonts({
+    "ArchivoBlack-Regular": require("./assets/fonts/ArchivoBlack-Regular.ttf"),
+    "OrelegaOne-Regular": require("./assets/fonts/OrelegaOne-Regular.ttf"),
+    "DidactGothic-Regular": require("./assets/fonts/DidactGothic-Regular.ttf"),
+    "Basic-Regular": require("./assets/fonts/Basic-Regular.ttf"),
+    "HiraKakuStdN-W8": require("./assets/fonts/HiraKakuStdN-W8.otf"),
+    "Inter-Medium": require("./assets/fonts/Inter-Medium.ttf"),
+    "HiraKakuProN-W3": require("./assets/fonts/HiraKakuProN-W3.otf"),
+    "HiraKakuPro-W3": require("./assets/fonts/HiraKakuPro-W3.otf"),
+    "Poppins-Regular": require("./assets/fonts/Poppins-Regular.ttf"),
+    "Poppins-Bold": require("./assets/fonts/Poppins-Bold.ttf"),
+  });
+
+  if (!fontsLoaded && !error) {
+    return null;
+  }
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator initialRouteName="Onboarding0">
+        <Stack.Screen name="Onboarding0" component={Onboarding0} options={{ headerShown: false }} />
+        <Stack.Screen name="Onboarding1" component={Onboarding1} options={{ headerShown: false }} />
+        <Stack.Screen name="Onboarding2" component={Onboarding2} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
